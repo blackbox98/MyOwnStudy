0150c44207857651c01688ff9c4908f89aad84aa
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2022-07-09 05:08:05 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int N, ans;
+	static int[] stair;
+	static int[] memo;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		stair = new int[301];
+		for (int i = 1; i <= N; i++) {
+			stair[i] = Integer.parseInt(br.readLine());
+		}
+		
+		ans = 0;
+		memo = new int[301];
+		dp();
+		System.out.println(ans);
+	}
+	private static void dp() {
+		memo[1] = stair[1];
+		memo[2] = stair[1] + stair[2];
+		
+		for (int i = 3; i <= N; i++) {
+			memo[i] = Math.max(memo[i - 3] + stair[i - 1], memo[i - 2]) + stair[i];
+		}
+		
+		ans = memo[N];
+	}
+}
