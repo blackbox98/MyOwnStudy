ce3dc69858c1cd7c5d2118ed9183763e9b18754f
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-01-10 17:58:38 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 11.05 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < food.length; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
             for (int j = 0; j < food[i] / 2; j++) {
-                answer.append(i);
+                sb.append(i);
             }
         }
-        String reverse = answer.reverse().toString();
-        answer.reverse();
-        answer.append(0).append(reverse);
-        return answer.toString();
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
