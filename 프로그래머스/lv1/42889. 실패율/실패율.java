5ccbba3cb7557fa56492e3fa20e8092984a94139
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-01-18 02:03:09 +0900

    [level 1] Title: 실패율, Time: 0.76 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        Map<Integer, Double> failure = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            failure.put(i, 0.0);
+        }
+        Arrays.sort(stages);
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+            int reached = 0;
+            for (int j = stages.length - 1 ; j >= 0; j--) {
+                if (i > stages[j]) break;
+                if (i <= stages[j]){
+                    reached++;
+                    if (i == stages[j]) cnt++;
+                }
+            }
+            if (reached == 0) {
+                failure.put(i, (double) 0);
+                continue;
+            }
+            failure.put(i, (double) cnt / reached);
+        }
+        List<Integer> list = new ArrayList<>(failure.keySet());
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return failure.get(o2).compareTo(failure.get(o1));
+            }
+        });
+        for (int i = 0; i < N; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
