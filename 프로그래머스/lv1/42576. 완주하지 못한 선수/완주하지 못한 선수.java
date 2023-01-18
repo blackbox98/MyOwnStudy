a71a9963c08dbcb070c800b621d04c920e343ca0
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-01-19 00:33:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.93 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,11 +1,25 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) return participant[i];
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String name : participant) map.put(name, map.getOrDefault(name, 0) + 1);
+        for (String name : completion) map.put(name, map.get(name) - 1);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                answer = entry.getKey();
+                break;
+            }
         }
-        return participant[participant.length - 1];
+        return answer;
+
+        /*
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+            for (int i = 0; i < completion.length; i++) {
+                if (!participant[i].equals(completion[i])) return participant[i];
+            }
+            return participant[participant.length - 1];
+        */
     }
 }
