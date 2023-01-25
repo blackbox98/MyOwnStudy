972bc3be598e684c95684277e96a2227e510d0be
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-01-25 13:12:27 +0900

    [level 1] Title: 평균 구하기, Time: 0.85 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -2,5 +2,7 @@ import java.util.Arrays;
 class Solution {
     public double solution(int[] arr) {
         return (double) Arrays.stream(arr).sum() / arr.length;
+//        return Arrays.stream(arr).average().orElse(0);
+//        return Arrays.stream(arr).average().getAsDouble();
     }
 }
