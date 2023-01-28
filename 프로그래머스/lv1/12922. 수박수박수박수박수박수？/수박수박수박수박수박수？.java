887db6160d0206b632870c745aed4eb14bf95e35
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-01-29 01:13:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,8 +2,7 @@ class Solution {
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) sb.append("수");
-            else sb.append("박");
+            sb.append(i % 2 == 0 ? "수" : "박");
         }
         return sb.toString();
     }
