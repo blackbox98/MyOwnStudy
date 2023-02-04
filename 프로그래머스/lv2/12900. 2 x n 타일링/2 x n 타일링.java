464a5364d19f02b0c2998881d59c1b1a034bf068
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-02-05 01:49:01 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.74 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int[] answer = new int[n + 1];
+        answer[1] = 1;
+        answer[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            answer[i] = (answer[i - 2] + answer[i - 1]) % 1000000007;
+        }
+        return answer[n];
+    }
+}
