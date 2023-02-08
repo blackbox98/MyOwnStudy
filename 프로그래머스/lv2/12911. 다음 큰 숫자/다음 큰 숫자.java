f468cbcd74a74abb9367c47fc1374f45d8e55b52
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-02-09 01:02:28 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int n) {
-        int n_one = Integer.bitCount(n);
+        int nCnt = Integer.bitCount(n);
         int answer = n + 1;
         while (true) {
-            int answer_one = Integer.bitCount(answer);
-            if (n_one == answer_one) break;
+            if (Integer.bitCount(answer) == nCnt) break;
             answer++;
         }
         return answer;
