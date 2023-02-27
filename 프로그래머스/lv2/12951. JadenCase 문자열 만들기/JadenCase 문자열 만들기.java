35b89f97154b4bdd02cb116bc7c2d85d5f3bbe47
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-02-28 00:05:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,18 +2,10 @@ class Solution {
     public String solution(String s) {
         StringBuilder answer = new StringBuilder();
         for (String str : s.toLowerCase().split(" ")) {
-            if (str.length() == 0) {
-                answer.append(' ');
-                continue;
-            } else if (str.charAt(0) - '0' > 9) {
-                answer.append((char) (str.charAt(0) - 32));
-                if (str.length() > 1) answer.append(str.substring(1));
-            } else {
-                answer.append(str);
-            }
-            answer.append(' ');
+            if (str.length() > 0) answer.append(str.substring(0, 1).toUpperCase()).append(str.substring(1));
+            answer.append(" ");
         }
-        if (s.substring(s.length() - 1).equals(" ")) return answer.toString();
-        return answer.deleteCharAt(answer.length() - 1).toString();
+        if (s.charAt(s.length() - 1) != ' ') answer.deleteCharAt(answer.length() - 1);
+        return answer.toString();
     }
 }
