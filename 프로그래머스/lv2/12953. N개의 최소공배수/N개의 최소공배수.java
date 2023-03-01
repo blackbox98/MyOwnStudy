ec0ac17d83585549a297a9766fb92f6a6f7753a5
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-03-01 23:47:59 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,29 +1,14 @@
 class Solution {
     public int solution(int[] arr) {
-        if (arr.length == 1) return arr[0];
-        int answer = 0;
-        int min = arr[0] > arr[1] ? arr[1] : arr[0];
-        int gcd = 0;
-        for (int j = min; j >= 1; j--) {
-            if (arr[0] % j == 0 && arr[1] % j == 0) {
-                gcd = j;
-                break;
-            }
-        }
-        answer = (arr[0] * arr[1]) / gcd;
-        min = answer;
-        for (int i = 2; i < arr.length; i++) {
-            min = min > arr[i] ? arr[i] : min;
-            gcd = 0;
-            for (int j = min; j >= 1; j--) {
-                if (answer % j == 0 && arr[i] % j == 0) {
-                    gcd = j;
-                    break;
-                }
-            }
-            answer = (answer * arr[i]) / gcd;
-            min = answer;
+        int answer = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            answer = (answer * arr[i]) / gcd(answer, arr[i]);
         }
         return answer;
+    }
+    
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
     }
 }
