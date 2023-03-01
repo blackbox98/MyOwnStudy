56bd64106cb999bd15ce60b41438ab79ba61f5e0
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-03-02 00:25:06 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.58 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,9 +4,8 @@ class Solution {
     public int solution(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (stack.isEmpty()) stack.push(c);
-            else if (stack.peek() == c) stack.pop();
-            else stack.push(c);
+            if (stack.isEmpty() || stack.peek() != c) stack.push(c);
+            else stack.pop();
         }
         return stack.isEmpty() ? 1 : 0;
     }
