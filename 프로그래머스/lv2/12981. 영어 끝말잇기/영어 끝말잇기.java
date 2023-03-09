7185e6c3859e0f4c2b12bb903991c6a7dceff7e0
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-03-09 19:32:08 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,24 +1,24 @@
-import java.util.Stack;
+import java.util.Set;
+import java.util.HashSet;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer;
-        Stack<String> stack = new Stack<>();
+        int[] answer = new int[] {0, 0};
+        Set<String> set = new HashSet<>();
         int num = 0;
         int turn = 0;
         char lastAlpha = words[0].charAt(0);
         boolean goal = true;
         for (String word : words) {
             if (++num % n == 1) turn++;
-            if (lastAlpha != word.charAt(0) || stack.contains(word)) {
+            if (lastAlpha != word.charAt(0) || set.contains(word)) {
                 goal = false;
                 break;
             }
-            stack.push(word);
+            set.add(word);
             lastAlpha = word.charAt(word.length() - 1);
         }
-        if (goal) answer = new int[] {0, 0};
-        else answer = new int[] {num % n == 0 ? n : num % n, turn};
+        if (!goal) answer = new int[] {num % n == 0 ? n : num % n, turn};
         return answer;
     }
 }
