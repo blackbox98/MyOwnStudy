39c3e620fe7329c7db073a88f2d47672a2f4d0bc
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-03-20 01:06:23 +0900

    [level 2] Title: 주식가격, Time: 31.69 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Price> stack = new Stack<>();
+        for (int i = 0; i < prices.length; i++) {
+            int price = prices[i];
+            if (stack.isEmpty()) {
+                stack.push(new Price(i, price));
+                continue;
+            }
+            if (stack.peek().price <= price) {
+                stack.push(new Price(i, price));
+            } else {
+                while (!stack.isEmpty() && stack.peek().price > price) {
+                    Price p = stack.pop();
+                    answer[p.idx] = i - p.idx;
+                }
+                stack.push(new Price(i, price));
+            }
+        }
+        if (!stack.isEmpty()) {
+            while (!stack.isEmpty()) {
+                Price p = stack.pop();
+                answer[p.idx] = prices.length - 1 - p.idx;
+            }
+        }
+        return answer;
+    }
+    
+    private static class Price {
+        int idx;
+        int price;
+
+        public Price(int idx, int price) {
+            this.idx = idx;
+            this.price = price;
+        }
+    }
+}
