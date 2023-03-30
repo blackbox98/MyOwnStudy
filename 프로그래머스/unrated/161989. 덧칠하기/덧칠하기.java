e22bdded184430dce496a0293d265b8b2593d244
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-03-31 01:22:49 +0900

    [unrated] Title: 덧칠하기, Time: 1.26 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,28 +1,11 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        if (section.length == 1) {
-            answer = section[0] / m;
-            if (section[0] % m > 0) answer++;
-        } else {
-            Queue<Integer> queue = new LinkedList<>();
-            for (int num : section) {
-                queue.offer(num);
-            }
-            while (!queue.isEmpty()) {
-                int start = queue.poll();
-                int end = start;
-                while (!queue.isEmpty()) {
-                    if (queue.peek() - start + 1 <= m) {
-                        end = queue.poll();
-                    } else break;
-                }
-                int area = end - start + 1;
-                answer += area / m;
-                if (area % m > 0) answer++;
+        int coverMax = 0;
+        for (int num : section) {
+            if (coverMax <= num) {
+                coverMax = num + m;
+                answer++;
             }
         }
         return answer;
