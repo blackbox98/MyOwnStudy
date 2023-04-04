a62fd17f38882633e5feeee3dbc91207eb786d9e
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-04-05 01:47:55 +0900

    [level 2] Title: 방문 길이, Time: 0.31 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.java
@@ -8,13 +8,15 @@ class Solution {
     public int solution(String dirs) {
         int answer = 0;
         int len = 5;
-        int size = len * 2 + 1;
+        int size = len * 2;
         int sr = 0;
         int sc = 0;
-        Set<Integer>[] setArrH = new Set[size];
+        Set<Integer>[] setArrH = new Set[size + 1];
         Set<Integer>[] setArrW = new Set[size];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size + 1; i++) {
             setArrH[i] = new HashSet<>();
+        }
+        for (int i = 0; i < size; i++) {
             setArrW[i] = new HashSet<>();
         }
         for (char way : dirs.toCharArray()) {
@@ -22,7 +24,6 @@ class Solution {
             int nr = sr + dir[idx][0];
             int nc = sc + dir[idx][1];
             if (nr >= -len & nr <= len && nc >= -len && nc <= len) {
-                int min = Math.min(sr + len, nr + len);
                 if (way == 'U' || way == 'D') setArrH[sc + len].add(Math.min(sr + len, nr + len));
                 else if (way == 'R' || way == 'L') setArrW[Math.min(sc + len, nc + len)].add(sr + len);
                 sr = nr;
@@ -32,6 +33,7 @@ class Solution {
         for (int i = 0; i < size; i++) {
             answer += setArrH[i].size() + setArrW[i].size();
         }
+        answer += setArrH[size].size();
         return answer;
     }
 }
