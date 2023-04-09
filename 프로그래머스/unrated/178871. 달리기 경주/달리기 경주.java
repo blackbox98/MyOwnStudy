68050ad39cc5c40e1cc9ad6f65f815fefffd1114
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-04-10 03:24:44 +0900

    [unrated] Title: 달리기 경주, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -1,31 +1,21 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = new String[players.length];
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<Integer, String> map2 = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < players.length; i++) {
-            map1.put(players[i], i);
-            map2.put(i, players[i]);
-
+            map.put(players[i], i);
         }
         for (String calling : callings) {
-            int ranking = map1.get(calling);
-            String back = map2.get(ranking - 1);
-            map1.put(back, ranking);
-            map1.put(calling, ranking - 1);
-            map2.put(ranking, back);
-            map2.put(ranking - 1, calling);
+            int ranking = map.get(calling);
+            String front = players[ranking - 1];
+            players[ranking] = front;
+            players[ranking - 1] = calling;
+            map.put(front, ranking);
+            map.put(calling, ranking - 1);
         }
-        Iterator<String> iter = map1.keySet().iterator();
-        while (iter.hasNext()) {
-            String player = iter.next();
-            answer[map1.get(player)] = player;
-        }
-        return answer;
+        return players;
     }
 }
