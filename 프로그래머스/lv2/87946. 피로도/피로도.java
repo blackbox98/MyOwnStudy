e93a0bde82fd1a05ca9a16d00c7a8ce6010d4f47
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-04-24 00:33:51 +0900

    [level 2] Title: 피로도, Time: 0.11 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -6,24 +6,19 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         size = dungeons.length;
         v = new boolean[size];
-        dfs(dungeons, k, 0, 0);
+        dfs(dungeons, k, 0);
         return answer;
     }
     
-    private static void dfs(int[][] dungeons, int k, int idx, int cnt) {
-        if (idx == size) {
-            answer = Math.max(answer, cnt);
-            return;
-        }
+    private static void dfs(int[][] dungeons, int k, int cnt) {
+        answer = Math.max(answer, cnt);
         for (int i = 0; i < size; i++) {
             int minReqFat = dungeons[i][0];
             int consumeFat = dungeons[i][1];
-            if (!v[i]) {
-                if (k >= minReqFat) {
-                    v[i] = true;
-                    dfs(dungeons, k - consumeFat, idx + 1, cnt + 1);
-                    v[i] = false;
-                } else dfs(dungeons, k, idx + 1, cnt);
+            if (!v[i] && k >= minReqFat) {
+                v[i] = true;
+                dfs(dungeons, k - consumeFat, cnt + 1);
+                v[i] = false;
             }
         }
     }
