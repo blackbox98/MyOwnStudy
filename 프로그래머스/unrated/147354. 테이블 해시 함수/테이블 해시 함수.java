8b19a43813679f5f150aeda7cf5cfa717ef1e83b
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-05-07 00:06:11 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.75 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -7,10 +7,9 @@ class Solution {
             if (o1[col - 1] == o2[col - 1]) return o2[0] - o1[0];
             return o1[col - 1] - o2[col - 1];
         });
-        for (int i = 0; i < row_end - row_begin + 1; i++) {
-            int idx = row_begin + i;
+        for (int i = row_begin; i <= row_end; i++) {
             int S_i = 0;
-            for (int n : data[idx - 1]) S_i += n % idx;
+            for (int n : data[i - 1]) S_i += n % i;
             answer ^= S_i;
         }
         return answer;
