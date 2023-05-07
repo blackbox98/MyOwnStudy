c610951bf6c2196ff4e5747be71a898b7b8bf340
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-05-08 00:11:51 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        int plus = 0;
+        String num = String.valueOf(storey);
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int n = num.charAt(i) - '0' + plus;
+            plus = 0;
+            if (n >= 6) {
+                answer += 10 - n;
+                plus = 1;
+            } else if (n == 5 && i - 1 >= 0 && (num.charAt(i - 1) - '0') >= 5) {
+                answer += 10 - n;
+                plus = 1;
+            } else answer += n;
+        }
+        answer += plus;
+        return answer;
+    }
+}
