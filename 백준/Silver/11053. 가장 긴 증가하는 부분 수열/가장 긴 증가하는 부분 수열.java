7a4e43a8e6bf591bf50f6b5bb184109aa71c5870
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-05-22 17:47:29 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14884 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 1;
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = 1;
+            for (int j = 1; j < i; j++) {
+                if (nums[i] > nums[j] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer);
+    }
+}
