973a1c82ef5fd0d6f6aab249d7c3db017b30cc01
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-06-23 01:35:26 +0900

    [Gold V] Title: 경쟁적 전염, Time: 776 ms, Memory: 106904 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -2,59 +2,67 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, K, S, X, Y;
+    static PriorityQueue<Virus> pq = new PriorityQueue<>();
+    static int[][] map;
+    static int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 우 하 좌 상
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        PriorityQueue<Virus> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.kind));
-        int[][] map = new int[N][N];
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
         for (int r = 0; r < N; r++) {
             st = new StringTokenizer(br.readLine());
             for (int c = 0; c < N; c++) {
                 map[r][c] = Integer.parseInt(st.nextToken());
-                pq.offer(new Virus(r, c, map[r][c]));
+                pq.offer(new Virus(r, c, map[r][c], 0));
             }
         }
         st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken()) - 1;
-        int Y = Integer.parseInt(st.nextToken()) - 1;
-        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 우 하 좌 상
-        while (S-- > 0) {
-            Queue<Virus> addQueue = new LinkedList<>();
-            while (!pq.isEmpty()) {
-                Virus virus = pq.poll();
-                int r = virus.r;
-                int c = virus.c;
-                int kind = virus.kind;
-                for (int[] d : dir) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-                    if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] == 0) {
-                        map[nr][nc] = kind;
-                        addQueue.offer(new Virus(nr, nc, kind));
-                    }
-                }
-            }
-            if (map[X][Y] > 0) break;
-            pq.addAll(addQueue);
-        }
+        S = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken()) - 1;
+        Y = Integer.parseInt(st.nextToken()) - 1;
+        if (S > 0) bfs();
         System.out.println(map[X][Y]);
     }
 
-    private static class Virus {
+    private static void bfs() {
+        while (!pq.isEmpty()) {
+            Virus virus = pq.poll();
+            for (int[] d : dir) {
+                int nr = virus.r + d[0];
+                int nc = virus.c + d[1];
+                if (nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] == 0 && virus.sec < S) {
+                    map[nr][nc] = virus.kind;
+                    pq.offer(new Virus(nr, nc, virus.kind, virus.sec + 1));
+                }
+            }
+            if (map[X][Y] > 0) break;
+        }
+    }
+
+    private static class Virus implements Comparable<Virus> {
         int r;
         int c;
         int kind;
+        int sec;
 
-        private Virus(int r, int c, int kind) {
+        private Virus(int r, int c, int kind, int sec) {
             this.r = r;
             this.c = c;
             this.kind = kind;
+            this.sec = sec;
+        }
+
+        @Override
+        public int compareTo(Virus o) {
+            return this.sec == o.sec ? Integer.compare(this.kind, o.kind) : Integer.compare(this.sec, o.sec);
         }
     }
 }
