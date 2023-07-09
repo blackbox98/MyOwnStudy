a531261226436e7f8d5673f6897548da7f9ffc09
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-07-09 23:44:50 +0900

    [Gold III] Title: 줄 세우기, Time: 528 ms, Memory: 96640 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
+        int[] dp = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            dp[num] = dp[num - 1] + 1;
+        }
+        Arrays.sort(dp);
+        System.out.println(N - dp[N]);
+    }
+}
