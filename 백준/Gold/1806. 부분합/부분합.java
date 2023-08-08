4e64cfdadb8ff0df55e6640198ebf96132178683
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-08-09 03:25:38 +0900

    [Gold IV] Title: 부분합, Time: 292 ms, Memory: 24224 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = Integer.MAX_VALUE;
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        while (left <= right && right <= N) {
+            if (sum >= S) {
+                answer = Math.min(answer, right - left);
+                sum -= nums[left++];
+            } else sum += nums[right++];
+        }
+        System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
+    }
+}
