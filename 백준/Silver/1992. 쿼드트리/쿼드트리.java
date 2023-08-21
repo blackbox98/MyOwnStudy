83078c92692126b208aa3a2f1881de6e14d0b5d3
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-08-22 01:06:00 +0900

    [Silver I] Title: 쿼드트리, Time: 136 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -3,75 +3,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-	static int[][] map;
-	static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		for (int n = 0; n < N; n++) {
-			String str = br.readLine();
-			for (int s = 0; s < str.length(); s++) {
-				map[n][s] = str.charAt(s) - '0';
-			}
-		}
-		QDTree(N, 0, 0);
-		System.out.println(sb);
-	}
-
-	private static void QDTree(int N, int r, int c) {
-		if (N == 2) {
-			int cnt0 = 0;
-			int cnt1 = 0;
-			sb.append("(");
-			for (int i = r; i < r + 2; i++) {
-				for (int j = c; j < c + 2; j++) {
-					if (map[i][j] == 0) {
-						cnt0++;
-					}
-					if (map[i][j] == 1) {
-						cnt1++;
-					}
-					sb.append(map[i][j]);
-				}
-
-			}
-			sb.append(")");
-			if (cnt0 == 4) {
-				sb.delete(sb.length() - 6, sb.length()).append(0);
-			}
-			if (cnt1 == 4) {
-				sb.delete(sb.length() - 6, sb.length()).append(1);
-			}
-			return;
-		}
-		sb.append("(");
-		QDTree(N / 2, r, c);
-		QDTree(N / 2, r, c + N / 2);
-		QDTree(N / 2, r + N / 2, c);
-		QDTree(N / 2, r + N / 2, c + N / 2);
-		sb.append(")");
-		checkOne();
-	}
-
-	private static void checkOne() {
-		int cnt0 = 0;
-		int cnt1 = 0;
-		for (int i = 0; i < 4; i++) {
-			if (sb.charAt(sb.length() - 2 - i) - '0' == 0) {
-				cnt0++;
-			}
-			if (sb.charAt(sb.length() - 2 - i) - '0' == 1) {
-				cnt1++;
-			}
-		}
-		if (cnt0 == 4) {
-			sb.delete(sb.length()-6, sb.length()).append(0);
-		}
-		if (cnt1 == 4) {
-			sb.delete(sb.length()-6, sb.length()).append(1);
-		}
-	}
+    static int[][] video;
+    static StringBuilder answer;
+    
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        video = new int[N][N];
+        char[] tmp;
+        int last = -1;
+        boolean flag = true;
+        for (int r = 0; r < N; r++) {
+            tmp = br.readLine().toCharArray();
+            for (int c = 0; c < N; c++) {
+                video[r][c] = tmp[c] - '0';
+                if (flag) {
+                    if (last == -1) last = video[r][c];
+                    else {
+                        if (last != video[r][c]) flag = false;
+                    }
+                }
+            }
+        }
+        if (flag) System.out.println(last);
+        else {
+            answer = new StringBuilder();
+            setArea(0, 0, N / 2);
+            System.out.println(answer);
+        }
+    }
+    
+    private static void setArea(int sr, int sc, int size) {
+        if (size < 1) return;
+        answer.append("(");
+        for (int nr = sr; nr < sr + size * 2; nr += size) {
+            for (int nc = sc; nc < sc + size * 2; nc += size) {
+                if (checkArea(nr, nc, video[nr][nc], size)) answer.append(video[nr][nc]);
+                else setArea(nr, nc, size / 2);
+            }
+        }
+        answer.append(")");
+    }
+    
+    private static boolean checkArea(int sr, int sc, int last, int size) {
+        for (int r = sr; r < sr + size; r++) {
+            for (int c = sc; c < sc + size; c++) {
+                if (last != video[r][c]) return false;
+            }
+        }
+        return true;
+    }
 }
