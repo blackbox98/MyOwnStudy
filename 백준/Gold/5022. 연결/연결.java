d44885f366e1a8ac3f96be6c6bc8d3213bc76b61
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-10-25 00:17:10 +0900

    [Gold I] Title: 연결, Time: 144 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Gold/5022. 연결/연결.java
+++ 백준/Gold/5022. 연결/연결.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,8 +11,8 @@ public class Main {
     static boolean[][] v;
     static Queue<Point> queue;
     static int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 우 하 좌 상
-    
-    public static void main(String args[]) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()) + 1;
@@ -27,7 +27,7 @@ public class Main {
         if (dist1 == Integer.MAX_VALUE && dist2 == Integer.MAX_VALUE) System.out.println("IMPOSSIBLE");
         else System.out.println(Math.min(dist1, dist2));
     }
-    
+
     private static int getDist(Point start1, Point start2, Point end1, Point end2) {
         map = new Point[N][M];
         map[start1.r][start1.c] = start1;
@@ -35,30 +35,30 @@ public class Main {
         v[start1.r][start1.c] = true;
         v[end1.r][end1.c] = true;
         v[end2.r][end2.c] = true;
-        
-        int startDist = bfs(start1, start2, end1, end2);
-        
+
+        int startDist = bfs(start1, start2);
+
         v = new boolean[N][M];
         v[start1.r][start1.c] = true;
         v[start2.r][start2.c] = true;
         Point next = map[start2.r][start2.c];
-        while(true) {
+        while (true) {
             v[next.r][next.c] = true;
             next = map[next.r][next.c];
             if (next.r == start1.r && next.c == start1.c) {
                 break;
             }
         }
-        
+
         v[end1.r][end1.c] = true;
-        
-        int endDist = bfs(end1, end2, start1, start2);
-        
+
+        int endDist = bfs(end1, end2);
+
         if (startDist == 0 || endDist == 0) return Integer.MAX_VALUE;
         else return startDist + endDist;
     }
-    
-    private static int bfs(Point start1, Point start2, Point end1, Point end2) {
+
+    private static int bfs(Point start1, Point start2) {
         queue = new LinkedList<>();
         queue.offer(start1);
         while (!queue.isEmpty()) {
@@ -80,16 +80,16 @@ public class Main {
         }
         return 0;
     }
-    
+
     private static boolean check(int r, int c) {
         return r < 0 || r >= N || c < 0 || c >= M;
     }
-    
+
     private static class Point {
         int r;
         int c;
         int dist;
-        
+
         private Point(int r, int c, int dist) {
             this.r = r;
             this.c = c;
