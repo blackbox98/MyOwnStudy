1db64afa069e961ce054c04f47287cf2f1653fed
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-10-30 00:34:42 +0900

    [Gold IV] Title: 목장 건설하기, Time: 532 ms, Memory: 75840 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.java
@@ -9,18 +9,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        char[][] map = new char[M][N];
+        int[][] map = new int[M][N];
         for (int r = 0; r < M; r++) {
-            String[] tmp = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int c = 0; c < N; c++) {
-                map[r][c] = tmp[c].charAt(0);
+                map[r][c] = Integer.parseInt(st.nextToken());
             }
         }
         int answer = 0;
         int[][] square = new int[M + 1][N + 1];
         for (int r = 1; r <= M; r++) {
             for (int c = 1; c <= N; c++) {
-                if (map[r - 1][c - 1] != '0') continue;
+                if (map[r - 1][c - 1] != 0) continue;
                 square[r][c] = Math.min(Math.min(square[r - 1][c], square[r][c - 1]), square[r - 1][c - 1]) + 1;
                 answer = Math.max(answer, square[r][c]);
             }
