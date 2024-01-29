cd6c58f5d262a608d46e764a63e31aaffcb9a2f4
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2024-01-29 21:06:41 +0900

    [Platinum IV] Title: 문명, Time: 1616 ms, Memory: 245348 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/14868. 문명/문명.java
+++ 백준/Platinum/14868. 문명/문명.java
@@ -1,120 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-    static int[][] map, distMap;
+    static int[] parent;
+    static int[][] map;
     static int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 우 하 좌 상
-    static PriorityQueue<int[]> pq;
     static Queue<int[]> queue;
-    static boolean[][] v;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+        parent = new int[K + 1];
+        for (int i = 1; i <= K; i++) {
+            parent[i] = i;
+        }
         map = new int[N][N];
-        for (int i = 0; i < K; i++) {
+        queue = new LinkedList<>();
+        for (int i = 1; i <= K; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken()) - 1;
             int c = Integer.parseInt(st.nextToken()) - 1;
-            map[r][c] = -1;
+            map[r][c] = i;
+            queue.offer(new int[]{r, c});
         }
-        distMap = new int[N][N];
-        pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
-        queue = new LinkedList<>();
-        int num = 1;
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                if (map[r][c] == -1) {
-                    grouping(r, c, num++);
-                }
-            }
-        }
-        int left = 0;
-        int right = 0;
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                right = Math.max(right, distMap[r][c]);
-            }
-        }
-        int answer = right;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (bfs(mid)) {
-                answer = Math.min(answer, mid);
-                right = mid - 1;
-            } else left = mid + 1;
+        int answer = 0;
+        joinMap();
+        while (K > 1) {
+            expansionMap();
+            answer++;
         }
         System.out.println(answer);
     }
 
-    private static void grouping(int sr, int sc, int num) {
-        queue.offer(new int[]{sr, sc});
-        v = new boolean[N][N];
-        v[sr][sc] = true;
-        while (!queue.isEmpty()) {
+    private static void joinMap() {
+        int size = queue.size();
+        for (int i = 0; !queue.isEmpty() && i < size; i++) {
             int[] cur = queue.poll();
-            map[cur[0]][cur[1]] = num;
+            int num = map[cur[0]][cur[1]];
+            queue.offer(cur);
             for (int[] d : dir) {
                 int nr = cur[0] + d[0];
                 int nc = cur[1] + d[1];
-                if (check(nr, nc) || v[nr][nc]) continue;
-                if (map[nr][nc] == -1) queue.offer(new int[]{nr, nc});
-                else if (map[nr][nc] == 0) pq.offer(new int[]{nr, nc, 0});
-                v[nr][nc] = true;
-            }
-        }
-        if (!pq.isEmpty()) getDist();
-    }
-
-    private static void getDist() {
-        v = new boolean[N][N];
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            if (v[cur[0]][cur[1]]) continue;
-            v[cur[0]][cur[1]] = true;
-            int nd = cur[2] + 1;
-            distMap[cur[0]][cur[1]] = nd;
-            for (int[] d : dir) {
-                int nr = cur[0] + d[0];
-                int nc = cur[1] + d[1];
-                if (check(nr, nc) || v[nr][nc] || map[nr][nc] != 0 || (distMap[nr][nc] != 0 && distMap[nr][nc] <= nd))
-                    continue;
-                pq.offer(new int[]{nr, nc, nd});
+                if (check(nr, nc) || map[nr][nc] == 0 || find(num) == find(map[nr][nc])) continue;
+                union(num, map[nr][nc]);
+                K--;
             }
         }
     }
 
-    private static boolean bfs(int mid) {
-        boolean flag = false;
-        v = new boolean[N][N];
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                if (v[r][c] || distMap[r][c] > mid) continue;
-                if (flag) return false;
-                queue.offer(new int[]{r, c});
-                v[r][c] = true;
-                while (!queue.isEmpty()) {
-                    int[] cur = queue.poll();
-                    for (int[] d : dir) {
-                        int nr = cur[0] + d[0];
-                        int nc = cur[1] + d[1];
-                        if (check(nr, nc) || v[nr][nc] || distMap[nr][nc] > mid) continue;
-                        queue.offer(new int[]{nr, nc});
-                        v[nr][nc] = true;
-                    }
+    private static void expansionMap() {
+        int size = queue.size();
+        for (int i = 0; !queue.isEmpty() && i < size; i++) {
+            int[] cur = queue.poll();
+            int cNum = map[cur[0]][cur[1]];
+            for (int[] d1 : dir) {
+                int nr = cur[0] + d1[0];
+                int nc = cur[1] + d1[1];
+                if (check(nr, nc) || map[nr][nc] != 0) continue;
+                queue.offer(new int[]{nr, nc});
+                map[nr][nc] = cNum;
+                for (int[] d2 : dir) {
+                    int nnr = nr + d2[0];
+                    int nnc = nc + d2[1];
+                    if (check(nnr, nnc) || map[nnr][nnc] == 0 || map[nr][nc] == map[nnr][nnc] || find(map[nr][nc]) == find(map[nnr][nnc]))
+                        continue;
+                    union(map[nr][nc], map[nnr][nnc]);
+                    K--;
+                    if (K == 1) break;
                 }
-                flag = true;
             }
         }
-        return flag;
     }
 
     private static boolean check(int r, int c) {
         return r < 0 || r >= N || c < 0 || c >= N;
+    }
+
+    private static void union(int a, int b) {
+        int A = find(a);
+        int B = find(b);
+        if (A != B) parent[B] = A;
+    }
+
+    private static int find(int a) {
+        return parent[a] == a ? a : (parent[a] = find(parent[a]));
     }
 }
