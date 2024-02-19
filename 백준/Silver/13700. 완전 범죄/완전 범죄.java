387bd8286183860bc4302a6e60d4d392708e3c79
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2024-02-19 11:45:45 +0900

    [Silver I] Title: 완전 범죄, Time: 248 ms, Memory: 18788 KB -BaekjoonHub

--- 백준/Silver/13700. 완전 범죄/완전 범죄.java
+++ 백준/Silver/13700. 완전 범죄/완전 범죄.java
@@ -1,105 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Set<Integer> office = new HashSet<>();
-    static int N, S, D, F, B, K;
-    static boolean[] visited;
-    
-    private static class Node {
-        // 위치와 이동횟수를 담을 변수
-        int index;
-        int count;
-
-        private Node(int index, int count) {
-            this.index = index;
-            this.count = count;
-        }
-
-        private int getIndex() {
-            return index;
-        }
-
-        private int getCount() {
-            return count;
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-
-        // 마포구 건물의 개수
-        N = Integer.parseInt(stringTokenizer.nextToken());
-
-        // 털린 금은방(시작 위치)
-        S = Integer.parseInt(stringTokenizer.nextToken());
-
-        // 대도 x의 집(목표 위치)
-        D = Integer.parseInt(stringTokenizer.nextToken());
-
-        // 앞으로 한번에 달릴 수 있는 건물 수
-        F = Integer.parseInt(stringTokenizer.nextToken());
-
-        // 뒤로 한번에 달릴 수 있는 건물 수
-        B = Integer.parseInt(stringTokenizer.nextToken());
-
-        // 경찰서의 개수
-        K = Integer.parseInt(stringTokenizer.nextToken());
-
-        visited = new boolean[N + 1];
-
-        // k가 0보다 크면 경찰서에 대한 정보를 담음
-        if (K > 0) {
-            stringTokenizer = new StringTokenizer(br.readLine());
-            while (stringTokenizer.hasMoreTokens()) {
-                office.add(Integer.valueOf(stringTokenizer.nextToken()));
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int F = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        if (K > 0) st = new StringTokenizer(br.readLine());
+        boolean[] v = new boolean[N + 1];
+        for (int i = 0; i < K; i++) {
+            v[Integer.parseInt(st.nextToken())] = true;
         }
-
-        bfs(new Node(S, 0));
-
+        System.out.println(bfs(N, S, D, F, B, v));
     }
 
-    static void bfs(Node node) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(node);
-
-        // 정답을 담을 변수
-        int answer = 0;
-
-        while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            if (currentNode.getIndex() == D) {
-                answer = currentNode.getCount();
-                break;
+    private static String bfs(int N, int S, int D, int F, int B, boolean[] v) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        pq.offer(new int[]{S, 0});
+        v[S] = true;
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (cur[0] == D) return cur[1] + "";
+            if (cur[0] - B >= 1 && !v[cur[0] - B]) {
+                pq.offer(new int[]{cur[0] - B, cur[1] + 1});
+                v[cur[0] - B] = true;
             }
-            // 현위치
-            int x = currentNode.getIndex();
-
-            visited[x] = true;
-
-            int frontMoved = x + F;
-            int backMoved = x - B;
-
-            // 이동했을 때 경찰서인지 검사
-            boolean frontMoveIsPolice = office.contains(frontMoved);
-            boolean backMoveIsPolice = office.contains(backMoved);
-
-            // frontMoveIsPolice 값
-            // true ==  앞으로 갔을 때 경찰서가 있음, false == 앞으로 갔을 때 경찰서가 없음
-            if (!frontMoveIsPolice && frontMoved <= N && !visited[frontMoved]) {
-                visited[frontMoved] = true;
-                queue.add(new Node(frontMoved, currentNode.getCount() + 1));
-            }
-			
-            if (!backMoveIsPolice && backMoved >= 1 && !visited[backMoved]) {
-                visited[backMoved] = true;
-                queue.add(new Node(backMoved, currentNode.getCount() + 1));
+            if (cur[0] + F <= N && !v[cur[0] + F]) {
+                pq.offer(new int[]{cur[0] + F, cur[1] + 1});
+                v[cur[0] + F] = true;
             }
         }
-        System.out.println(answer == 0 ? "BUG FOUND" : answer);
+        return "BUG FOUND";
     }
 }
