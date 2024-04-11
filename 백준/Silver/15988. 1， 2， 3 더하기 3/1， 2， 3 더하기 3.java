0a5877ddc885129b8984f2a2d0218bcba26f3c49
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2024-04-12 06:01:22 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 156 ms, Memory: 22172 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long[] dp = new long[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < 1000001; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+        }
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
